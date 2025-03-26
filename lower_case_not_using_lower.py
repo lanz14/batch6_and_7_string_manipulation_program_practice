d4f3dc5dffb74f6c5ba3d9cef5136a445784eca0
Lance <enaganlance@gmail.com>
2025-03-27 01:34:01 +0800

    user's input

--- lower_case_not_using_lower.py
+++ lower_case_not_using_lower.py
@@ -1,6 +1,8 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # user's input
+character_inputs = str(input("Input a text: "))
+
 # set a variable to an empty string
 # check from A to Z if there is a capital letter
 # convert capital into lower case
