431680d7876293c5a8ff97890135bd51d5d8a2ea
Lance <enaganlance@gmail.com>
2025-03-26 23:55:12 +0800

    user input

--- remove_characters_not_using_removeprefix.py
+++ remove_characters_not_using_removeprefix.py
@@ -1,5 +1,7 @@
 # Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
 # user's input
+user_input = str(input("Input any character: "))
+
 # ask the user what string to be removed at the beginning
 # print output
