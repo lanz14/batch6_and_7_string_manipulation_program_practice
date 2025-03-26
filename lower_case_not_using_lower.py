ba79e23a39d1a835842e354a9aa3539b95687a55
Lance <enaganlance@gmail.com>
2025-03-27 01:32:58 +0800

    pseudocode

---
+++ lower_case_not_using_lower.py
@@ -0,0 +1,7 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# user's input
+# set a variable to an empty string
+# check from A to Z if there is a capital letter
+# convert capital into lower case
+# print output
