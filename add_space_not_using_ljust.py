a315364d362d8bb9be5ab7e162cac052d83953a7
Lance <enaganlance@gmail.com>
2025-03-27 17:39:29 +0800

    user input and the length wanted of the user

--- add_space_not_using_ljust.py
+++ add_space_not_using_ljust.py
@@ -1,6 +1,10 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # user's input
+user_input = str(input("Enter a string: "))
+
 # ask the user how many spaces to add at the end
+length = int(input("Enter the total length: "))
+
 # add space to the end with specified number
 # print output
