d4617008e028bea0d4613b2f186530d9ab50ae08
Lance <enaganlance@gmail.com>
2025-03-27 19:28:34 +0800

    pseudocode

---
+++ center_function.py
@@ -0,0 +1,5 @@
+# Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# user's input
+# ask the user the amount of length for each side for centering the string
+# apply the length to the string then print the output
