841d4b0b6fedf5a63b99dd07015ad58e4ca694bf
Lance <enaganlance@gmail.com>
2025-03-27 20:04:57 +0800

    reverse case the letters

--- reverse_case_not_using_swapcase.py
+++ reverse_case_not_using_swapcase.py
@@ -1,5 +1,20 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # user's input
+user_input = str(input("Input a string: "))
+
 # reverse case the letters
+reverse_case = ""
+
+for letter in user_input:
+    if 'a' <= letter <= 'z':
+
+        reverse_case += chr(ord(letter) - 32)
+
+    elif 'A' <= letter <= 'Z':
+        reverse_case += chr(ord(letter) + 32)
+        
+    else:
+        reverse_case += letter
+
 # print the reverse cased letters
