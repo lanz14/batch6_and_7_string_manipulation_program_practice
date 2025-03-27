511c2e11011ea02ffc82a75b6ba7239a1d4d22b5
Lance <enaganlance@gmail.com>
2025-03-27 20:53:43 +0800

    pseudocode

---
+++ capitalize_first_letter.py
@@ -0,0 +1,5 @@
+# Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+# user's input
+# upper the first character and then lower the rest of the string
+# print output
