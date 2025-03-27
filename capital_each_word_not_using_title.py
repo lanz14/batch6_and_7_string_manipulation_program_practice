7c34e814e952579ad1618303c8644675e2debded
Lance <enaganlance@gmail.com>
2025-03-27 21:18:54 +0800

    user's input + use split function

--- capital_each_word_not_using_title.py
+++ capital_each_word_not_using_title.py
@@ -1,6 +1,10 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # user's input
+user_input = str(input("Input a sentence: "))
+
 # using split function to the user input to identify each word
+sentence = user_input.split()
+
 # for each word upper case the first letter
 # print output
