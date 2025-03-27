3ae235f7e30a9d8a50768e16db9f547e6c094993
Lance <enaganlance@gmail.com>
2025-03-27 19:30:08 +0800

    user's input, then ask user for the length of each side

--- center_function.py
+++ center_function.py
@@ -1,5 +1,9 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # user's input
+user_input = str(input("Input any string: "))
+
 # ask the user the amount of length for each side for centering the string
+length = int(input("The length of each side for centering: "))
+
 # apply the length to the string then print the output
