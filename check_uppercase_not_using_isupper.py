5e42585c89f36a379aa80ed3d442029073dcf0cb
Lance <enaganlance@gmail.com>
2025-03-27 15:03:34 +0800

    check if all upper case, print true or false

--- check_uppercase_not_using_isupper.py
+++ check_uppercase_not_using_isupper.py
@@ -6,5 +6,15 @@ input = str(input("Input a text: "))
 # check if the inputs are in all upper case
 upper_case = True
 
+for letter in input:
+    if "a" <= letter <= "z":
+        upper_case = False
+        break 
+
 # print "True" if the inputs are all in upper case
+if upper_case:
+    print("True")
+
 # print "False" if the inputs are not all in upper case
+else:
+    print("False")
