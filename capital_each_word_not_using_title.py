e4035434436928858be4f326dbbdb62904152562
Lance <enaganlance@gmail.com>
2025-03-27 21:20:48 +0800

    capitalize first letter for each word, then print

--- capital_each_word_not_using_title.py
+++ capital_each_word_not_using_title.py
@@ -7,4 +7,12 @@ user_input = str(input("Input a sentence: "))
 sentence = user_input.split()
 
 # for each word upper case the first letter
+upper = []
+for word in sentence:
+    if word: 
+        upper.append(word[0].upper() + word[1:].lower())
+
+output = ' '.join(upper)
+
 # print output
+print(f"\n{output}")
