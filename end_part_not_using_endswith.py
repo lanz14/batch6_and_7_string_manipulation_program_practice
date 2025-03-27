15bdf66de67f8c1250838b39755ddb92f73cc4fe
Lance <enaganlance@gmail.com>
2025-03-27 15:32:11 +0800

    user input and ask user what ending to check

--- end_part_not_using_endswith.py
+++ end_part_not_using_endswith.py
@@ -1,6 +1,10 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # user's input
+user_input = str(input("Input a text: "))
+
 # ask the user what ending characters to check
+end_characters = str(input("Check the end characters: "))
+
 # check the end characters
 # print "True" or "False"
