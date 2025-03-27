ab6bb91afdfa53bad23fb72bf2b5bd38a8e49388
Lance <enaganlance@gmail.com>
2025-03-27 15:30:41 +0800

    pseudocode

---
+++ end_part_not_using_endswith.py
@@ -0,0 +1,6 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# user's input
+# ask the user what ending characters to check
+# check the end characters
+# print "True" or "False"
