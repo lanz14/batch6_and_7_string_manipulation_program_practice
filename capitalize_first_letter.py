6533e3a3b639708fdc3c9153ea64cce2fa78478d
Lance <enaganlance@gmail.com>
2025-03-27 20:55:09 +0800

    user's input + convert the first character to upper case

--- capitalize_first_letter.py
+++ capitalize_first_letter.py
@@ -1,5 +1,11 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 # user's input
+user_input = str(input("Input a string: "))
+
 # upper the first character and then lower the rest of the string
+while len(user_input):
+    convert = user_input[0].upper() + user_input[1:].lower()
+    break
+
 # print output
