37ea4348abbc6e8aa1ff12a5385c5d49306e0ea5
Lance <enaganlance@gmail.com>
2025-03-27 14:59:59 +0800

    user input and assume the input is true

--- check_uppercase_not_using_isupper.py
+++ check_uppercase_not_using_isupper.py
@@ -1,6 +1,10 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # user's input
+input = str(input("Input a text: "))
+
 # check if the inputs are in all upper case
+upper_case = True
+
 # print "True" if the inputs are all in upper case
 # print "False" if the inputs are not all in upper case
