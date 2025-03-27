9ef52f85cd6a548a8f91ebfac60b26b94257feeb
Lance <enaganlance@gmail.com>
2025-03-27 21:17:58 +0800

    pseudocode

---
+++ capital_each_word_not_using_title.py
@@ -0,0 +1,6 @@
+# Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# user's input
+# using split function to the user input to identify each word
+# for each word upper case the first letter
+# print output
