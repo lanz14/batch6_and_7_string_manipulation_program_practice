8cbc7b8595f70574502609ed71dc63daa7b384cb
Lance <enaganlance@gmail.com>
2025-03-29 22:33:48 +0800

    pseudocode

---
+++ batch_7_string_manipulation_practice/without_using_removesuffix.py
@@ -0,0 +1,5 @@
+# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
+# user's input
+# ask the user what character at the end of the string to be removed
+# print output
