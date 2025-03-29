832e26374df13ec533fe2871100589375f6b361d
Lance <enaganlance@gmail.com>
2025-03-29 21:42:26 +0800

    user input, and ask the space length

--- batch_7_string_manipulation_practice/without_using_rstrip.py
+++ batch_7_string_manipulation_practice/without_using_rstrip.py
@@ -1,7 +1,12 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # user's input with spaces at the end of the string
+user_input = str(input("Input a string with spaces at the end: "))
+
 # ask the user the length of the spaces at the beginning to implement rstrip function
+length = int(input("Number of spaces at the beginning (integers): "))
+spaces = " " * length
+
 # add space to the beginning with specified length
 # remove space at the end of the string
 # print output
