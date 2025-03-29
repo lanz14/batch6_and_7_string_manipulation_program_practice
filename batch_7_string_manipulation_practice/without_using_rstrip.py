9bb6413b4563d6618ea53383c0433e1b8c21d2de
Lance <enaganlance@gmail.com>
2025-03-29 19:34:41 +0800

    pseudocode

---
+++ batch_7_string_manipulation_practice/without_using_rstrip.py
@@ -0,0 +1,7 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# user's input with spaces at the end of the string
+# ask the user the length of the spaces at the beginning to implement rstrip function
+# add space to the beginning with specified length
+# remove space at the end of the string
+# print output
