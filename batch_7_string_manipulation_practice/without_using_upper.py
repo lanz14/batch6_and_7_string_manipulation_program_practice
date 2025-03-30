07cfd9ec932e264475b540516c60bf2276bc4bc0
Lance <enaganlance@gmail.com>
2025-03-30 17:22:57 +0800

    user input, and convert lower case to upper case

--- batch_7_string_manipulation_practice/without_using_upper.py
+++ batch_7_string_manipulation_practice/without_using_upper.py
@@ -1,5 +1,17 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # user's input
+user_input = str(input("Enter a string: "))
+
 # identify small letters, then convert to uppercase
+result = ""
+
+for lower in user_input:
+    if 'a' <= lower <= 'z':
+        uppercase_char = chr(ord(lower) - 32)
+        result += uppercase_char
+
+    else:
+        result += lower
+
 # print output
