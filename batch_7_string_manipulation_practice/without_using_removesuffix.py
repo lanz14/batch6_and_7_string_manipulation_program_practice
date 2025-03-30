c90575224f3cd5535714e2a48a0ffa78709adffc
Lance <enaganlance@gmail.com>
2025-03-30 13:14:35 +0800

    user input, and ask the user what string to be removed

--- batch_7_string_manipulation_practice/without_using_removesuffix.py
+++ batch_7_string_manipulation_practice/without_using_removesuffix.py
@@ -1,5 +1,9 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 # user's input
+user_input = str(input("Input a string: "))
+
 # ask the user what character at the end of the string to be removed
+remove = str(input("String to be removed at the end?: "))
+
 # print output
