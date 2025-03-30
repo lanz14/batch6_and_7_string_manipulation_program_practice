134cbabb553e8b4430b0f40c8bbe31c74a74dd62
Lance <enaganlance@gmail.com>
2025-03-30 18:49:23 +0800

    pseudocode

---
+++ batch_7_string_manipulation_practice/without_using_rjust.py
@@ -0,0 +1,5 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# user input
+# ask the user the length of space character added at the beginning
+# implement the length and print the output
