81311e5206e3228f0fc2131a6e7723b17828902c
Lance <enaganlance@gmail.com>
2025-03-30 18:26:11 +0800

    user input, and ask user

--- batch_7_string_manipulation_practice/without_using_startswith.py
+++ batch_7_string_manipulation_practice/without_using_startswith.py
@@ -1,5 +1,9 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # user input
+user_input = str(input("Input a string: "))
+
 # ask the user characters to check at beginning
+check = str(input("Check the beginning of the string: "))
+
 # print if True or False
