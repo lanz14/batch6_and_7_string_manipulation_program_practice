1f97a16b361766f9304fc043ace965758ffec4d9
Lance <enaganlance@gmail.com>
2025-03-30 18:52:23 +0800

    user input, and ask the user for space length

--- batch_7_string_manipulation_practice/without_using_rjust.py
+++ batch_7_string_manipulation_practice/without_using_rjust.py
@@ -1,5 +1,10 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # user input
+user_input = str(input("Input a string: "))
+
 # ask the user the length of space character added at the beginning
+space_length = int(input("Number of space length (integers):"))
+spaces = " " * space_length
+
 # implement the length and print the output
