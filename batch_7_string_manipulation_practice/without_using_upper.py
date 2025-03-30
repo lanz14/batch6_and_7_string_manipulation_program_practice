1e8a7b66ad329cc1d35797fffc9b43c0d66ea19c
Lance <enaganlance@gmail.com>
2025-03-30 17:20:50 +0800

    pseudocode

---
+++ batch_7_string_manipulation_practice/without_using_upper.py
@@ -0,0 +1,5 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# user's input
+# identify small letters, then convert to uppercase
+# print output
