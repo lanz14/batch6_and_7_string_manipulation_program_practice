9db9ddd279cc6d45531ef2b430a8953a80285609
Lance <enaganlance@gmail.com>
2025-03-30 17:43:11 +0800

    user input, check if all lower case

--- batch_7_string_manipulation_practice/without_using_islower.py
+++ batch_7_string_manipulation_practice/without_using_islower.py
@@ -1,6 +1,15 @@
 # Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 
 # user's input
+user_input = str(input("Input a text: "))
+
 # check if the inputted string is all in lower case
+lower_case = True
+
+for letter in user_input:
+    if "A" <= letter <= "Z":
+        lower_case = False
+        break
+
 # print "True" if the input is all in lower case
 # print "False" if not all input is in lower case
