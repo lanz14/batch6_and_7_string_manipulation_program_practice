23d88bdb77aca78feb43650183db4bc932bdaad0
Lance <enaganlance@gmail.com>
2025-03-31 23:51:50 +0800

    pseudocode

---
+++ batch_7_string_manipulation_practice/without_using_index.py
@@ -0,0 +1,6 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# user's input
+# ask the user what substring to find
+# find the substring
+# print output
