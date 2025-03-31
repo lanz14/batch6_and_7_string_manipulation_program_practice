04e7057a1ca0b9affe8ce1c0fe950f2d4252a21b
Lance <enaganlance@gmail.com>
2025-03-31 23:32:14 +0800

    alert the user if not single character, else, print output

--- batch_7_string_manipulation_practice/without_using_count.py
+++ batch_7_string_manipulation_practice/without_using_count.py
@@ -7,4 +7,13 @@ user_input = input("Input a string: ")
 char_count = input("Character to count (e.g. letter 'a'): ")
 
 # if the user did not enter a single character, it will alert the user to input only single character to count
+if len(char_count) != 1:
+    print("\nPlease enter only single character from a-z.")
+
 # if the user input a single character, it will print the amount of times it appeared
+else:
+    counter = 0
+    for char in user_input:
+        if char == char_count:
+            counter += 1
+    print(f"\n'{char_count}' appears {counter} times.")
