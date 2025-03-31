bcf3c38fa26ce789905da0f7c3c48d70edec320d
Lance <enaganlance@gmail.com>
2025-04-01 00:05:13 +0800

    find the index

--- batch_7_string_manipulation_practice/without_using_rindex.py
+++ batch_7_string_manipulation_practice/without_using_rindex.py
@@ -7,4 +7,11 @@ user_input = input("Input a sentence: ")
 sub_str = input("Input a substring to find: ")
 
 # find the substring starting from the end
+result = -1
+
+for i in range(len(user_input) - len(sub_str), -1, -1):
+    if user_input[i:i+len(sub_str)] == sub_str:
+        result = i
+        break
+
 # print output
