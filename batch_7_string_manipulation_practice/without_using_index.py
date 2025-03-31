59d052a43452e53e798e355fe9611d0290040911
Lance <enaganlance@gmail.com>
2025-03-31 23:54:00 +0800

    user input, and ask user what substring to find

--- batch_7_string_manipulation_practice/without_using_index.py
+++ batch_7_string_manipulation_practice/without_using_index.py
@@ -1,6 +1,10 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # user's input
+user_input = input("Input a sentence: ")
+
 # ask the user what substring to find
+sub_str = input("Input a substring to find: ")
+
 # find the substring
 # print output
