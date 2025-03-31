cb7618aada04ad3cf6ddeb58f4dcf5d6a8af9433
Lance <enaganlance@gmail.com>
2025-03-31 20:47:43 +0800

    pseudocode

---
+++ batch_7_string_manipulation_practice/without_using_zfill.py
@@ -0,0 +1,6 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+# user's input
+# ask the user what is the length
+# match the number of length to the character by adding zeros
+# print output
