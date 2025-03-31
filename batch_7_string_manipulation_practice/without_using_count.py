527d6b86378d7705ac668dfb6a4983e78115e849
Lance <enaganlance@gmail.com>
2025-03-31 22:26:29 +0800

    pseudocode

---
+++ batch_7_string_manipulation_practice/without_using_count.py
@@ -0,0 +1,6 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# user's input
+# ask the user a single character to count within a string
+# if the user did not enter a single character, it will alert the user to input only single character to count
+# if the user input a single character, it will print the amount of times it appeared
