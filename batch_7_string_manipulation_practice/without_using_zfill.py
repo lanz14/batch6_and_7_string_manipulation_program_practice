c8a668e5fecf58b41633afab6b38dec7281e1557
Lance <enaganlance@gmail.com>
2025-03-31 21:16:22 +0800

    user input, and length

--- batch_7_string_manipulation_practice/without_using_zfill.py
+++ batch_7_string_manipulation_practice/without_using_zfill.py
@@ -1,6 +1,10 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # user's input
+user_input = input("Input a string: ")
+
 # ask the user what is the length
+length = int(input("Length: "))
+
 # match the number of length to the character by adding zeros
 # print output
