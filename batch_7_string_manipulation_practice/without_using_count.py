cbf2dc8f5a109822caafbd46aa2a075b59efa5be
Lance <enaganlance@gmail.com>
2025-03-31 23:31:05 +0800

    user input, and ask user what character to count

--- batch_7_string_manipulation_practice/without_using_count.py
+++ batch_7_string_manipulation_practice/without_using_count.py
@@ -1,6 +1,10 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # user's input
+user_input = input("Input a string: ")
+
 # ask the user a single character to count within a string
+char_count = input("Character to count (e.g. letter 'a'): ")
+
 # if the user did not enter a single character, it will alert the user to input only single character to count
 # if the user input a single character, it will print the amount of times it appeared
