78df0456582ffe35bef714299abb30896d6b7436
Lance <enaganlance@gmail.com>
2025-04-01 00:04:35 +0800

    user input, then ask user what substring to find

--- batch_7_string_manipulation_practice/without_using_rindex.py
+++ batch_7_string_manipulation_practice/without_using_rindex.py
@@ -1,6 +1,10 @@
 # Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
 # user's input
+user_input = input("Input a sentence: ")
+
 # ask the user what substring to find
+sub_str = input("Input a substring to find: ")
+
 # find the substring starting from the end
 # print output
